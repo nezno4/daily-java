67f6860e205fbad1fb42f2316ab97c2c2db58f66
Nez <zenitechq@gmail.com>
2024-12-02 16:47:53 +0100

    hotfix(CalculatorTest.java): test deepsource on test in calculator

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,5 +8,11 @@ class CalculatorTest {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3));
     }
+
+    @Test
+    void testSubtract() {
+        Calculator calculator = new Calculator();
+        assertEquals(-1, calculator.subtract(2, 3));
+    }
 }
 
